3113101a56f05b517e3f2cc30671cdbb32352c12
鸡毛 <shihao.gu@perfma.com>
2019-11-09 13:04:23 +0800

    应用真正的c库

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { Pwm } from './pwm';
 const iPwm = new Pwm();
 
 iPwm.WiringPiInit();
-iPwm.PWMInit(0, 1, 2);
-iPwm.PWMSet(0, 111);
+// iPwm.PWMInit(0, 1, 2);
+// iPwm.PWMSet(0, 111);
--- src/pwm.c
+++ src/pwm.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-// #include <wiringPi.h>
-// #include <softPwm.h>
+#include <wiringPi.h>
+#include <softPwm.h>
 
 // 初始化wiringPi
 void WiringPiInit() {
-  // wiringPiSetup();
-  printf("WiringPiInit\n");
+  wiringPiSetup();
+  // printf("WiringPiInit\n");
 }
 
 // 初始化某一GPIO上的PWM功能
 void PWMInit(int gpio, int min, int max) {
-  // softPwmCreate(gpio, min, max);
-  printf("PWMInit %d %d %d\n", gpio, min, max);
+  softPwmCreate(gpio, min, max);
+  // printf("PWMInit %d %d %d\n", gpio, min, max);
 }
 
 // 设置某一GPIO上的PWM
 void PWMSet(int gpio, int value) {
-  // softPwmWrite(gpio, value);
-  printf("PWMSet %d %d\n", gpio, value);
+  softPwmWrite(gpio, value);
+  // printf("PWMSet %d %d\n", gpio, value);
 }
